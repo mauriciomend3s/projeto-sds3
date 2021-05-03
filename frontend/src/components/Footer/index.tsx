be7ff50f6694d7e77ea9d5d2d75efbd5aa15329d
Maurício Mendes <mauricio.mend3s@gmail.com>
2021-05-03 15:09:09 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/mauriciomend3s" target="_blank" rel="noreferrer">Maurício Mendes</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://github.com/mauriciomend3s" target="_blank" rel="noreferrer">GitHub</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
