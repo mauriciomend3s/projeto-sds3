f2c46d0fb7a3b6880809a5e79b3ea8e0e7feccef
Maurício Mendes <mauricio.mend3s@gmail.com>
2021-05-08 20:51:49 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const Dashboard = () => {
+    return (
+       
+          
+            <Footer />
+        
+    );
+}
+
+export default Dashboard;
